7e557a895092ecdad80c344eaf45b7a26fe95f11
Chandramani321 <88945200+Chandramani321@users.noreply.github.com>
2021-12-30 12:02:31 +0530

    Create Random.java

---
+++ Random.java
@@ -0,0 +1,8 @@
+// Generate 5 nos random 
+public class Random {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println((int) (Math.random() * 1000));
+		}
+	}
+}
